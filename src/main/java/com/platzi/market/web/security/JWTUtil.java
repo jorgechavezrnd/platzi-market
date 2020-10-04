88805732d8051a39fe7b2f29e653b12c62dbbd3c
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-10-04 18:57:00 -0400

    AuthController for authenticate users and generate JWTs added

--- src/main/java/com/platzi/market/web/security/JWTUtil.java
+++ src/main/java/com/platzi/market/web/security/JWTUtil.java
@@ -3,11 +3,12 @@ package com.platzi.market.web.security;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
+@Component
 public class JWTUtil {
-
     private static final String KEY = "pl4tz1";
 
     public String generateToken(UserDetails userDetails) {
@@ -15,5 +16,4 @@ public class JWTUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // The token will be valid for 10 hours
                 .signWith(SignatureAlgorithm.HS256, KEY).compact();
     }
-
 }
